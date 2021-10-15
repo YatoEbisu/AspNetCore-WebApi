6002234f67b9a6b3ddb9375a9c5ebb8a7c9b1711
Breno SF <BrenoSilvaFortunato@gmail.com>
2021-10-15 12:07:10 -0300

    feat: add base repository

---
+++ WebApi/src/Store.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task InsertAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task RemoveAsync(T obj);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T> FindOneAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ WebApi/src/Store.Infra.Data/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Domain.Entities;
+using Store.Domain.Interfaces;
+using Store.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Infra.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        private readonly StoreContext _context;
+        public Repository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public async Task<T> FindOneAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AsNoTracking().FirstOrDefaultAsync(predicate); ;
+        }
+
+        public async Task InsertAsync(T obj)
+        {
+            _context.Set<T>().Add(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(T obj)
+        {
+            _context.Set<T>().Remove(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T obj)
+        {
+            _context.Set<T>().Update(obj);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

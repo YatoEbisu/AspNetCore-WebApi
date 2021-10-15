52d1011ef6817417af02c9ce4bfec5dbefdf4d12
Breno SF <BrenoSilvaFortunato@gmail.com>
2021-10-15 15:13:39 -0300

    feat: create entities

--- WebApi/src/Store.Domain/Entities/Order.cs
+++ WebApi/src/Store.Domain/Entities/Order.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Store.Domain.Entities
 {
-    public class Entity
+    public class Order : EntityBase
     {
+        public Guid ProductId { get; set; }
+        public Guid UserId {  get; set; }
+        public int Mount {  get; set; }
     }
 }
---
+++ WebApi/src/Store.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities
+{
+    internal class User
+    {
+        public User(string login, string password)
+        {
+            Login = login;
+            Password = password;
+            CreatedAt = DateTime.Now;
+        }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}

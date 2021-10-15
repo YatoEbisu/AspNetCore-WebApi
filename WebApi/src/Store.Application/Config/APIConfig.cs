c31363e9752836fa4d200f9df5f1bf2096b08125
Breno SF <BrenoSilvaFortunato@gmail.com>
2021-10-15 15:43:23 -0300

    config startup

---
+++ WebApi/src/Store.Application/Config/APIConfig.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Store.Infra.Data.Context;
+
+namespace Store.Application.Config
+{
+    public static class APIConfig
+    {
+        public static IServiceCollection AddAPIConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<StoreContext>(opt => opt
+             .UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+         );
+            return services;
+        }
+
+        public static IApplicationBuilder UseAPIConfig(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+
+    }
+}
--- WebApi/src/Store.Application/Startup.cs
+++ WebApi/src/Store.Application/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Store.Application.Config;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,31 +23,15 @@ namespace Store.Application
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddAPIConfig(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseAPIConfig(env);
         }
     }
 }
